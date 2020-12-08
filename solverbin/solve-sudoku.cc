552a42b962ce9548b5e20df7231b027ed496d196
Haoran Zhou <hzhou127@cs.umd.edu>
2020-12-07 20:58:27 -0500

    add solverbin

--- solverbin/solve-sudoku.cc
+++ solverbin/solve-sudoku.cc
@@ -1,9 +1,46 @@
-// A tool for solving sudoku problems from file
-// Usage:
+// A tool for solve sudoku problems using serial solver
+// Usage: solve-sudoku <intput_file (sudoku problems)> <output_file (answers)>
 //
 
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main() 
-{
-    return 0;
+#include "solver/solver-serial.h"
+#include "generator/file-parser.h"
+
+
+int main(int argc, char *argv[]) {
+    try {
+        using namespace sudoku;
+        const char *usage = 
+            "Solve sudoku problems and export them to a file. \n"
+            "Usage: solve-sudoku <intput_file (sudoku problems)> <output_file (answers) \n";
+        
+        if (argc != 3) {
+            fprintf(stderr, "%s", usage);
+            exit(1);
+        }
+
+        std::string infile(argv[1]);
+        std::string outfile(argv[2]);
+
+        Solvable *read_sudoku = nullptr;
+        SudokuAnswer answer;
+
+        FileParser fp;
+        fp.ReadFromFile(infile, read_sudoku);
+
+        SolverSerial *solver = SolverSerial::GetInstance();
+
+        
+        solver->Solve(*read_sudoku, answer);
+
+        fp.WriteToFile(outfile, &answer);
+        delete read_sudoku;
+
+    } catch (const std::exception &e) {
+        std::cerr << e.what();
+        return -1;
+    }
 }
