055e8c10ab6fbef75875225c8610f1e7a6a1fd91
Haoran Zhou <hzhou127@cs.umd.edu>
2020-12-07 20:30:01 -0500

    Change to only generating one since solver-mpi only supports solving one at a time

--- generator/file-parser.h
+++ generator/file-parser.h
@@ -15,21 +15,13 @@ namespace sudoku {
 
 class FileParser {
 
-    using solvable_iter = std::vector<Solvable*>::iterator;
-    using validatable_iter = std::vector<Validatable*>::iterator;
 
 public:
-    static void WriteToFile(std::string out_file, std::vector<Solvable*> *in_vec);
-    static void WriteToFile(std::string out_file, std::vector<Validatable*> *in_vec);
+    static void WriteToFile(std::string out_file, Solvable *sudoku);
+    static void WriteToFile(std::string out_file, Validatable* sudoku);
 
     // caller responsible for free returned value
-    static void ReadFromFile(std::string input_file, std::vector<Solvable*> *&ret);
-
-private:
-    static void WriteOne(Solvable *data, std::ofstream &file);
-    static void WriteOne(Validatable *data, std::ofstream &file);
-
-    static bool ReadOne(Sudoku *&data, std::ifstream &file);
+    static void ReadFromFile(std::string input_file, Solvable *&ret);
 
 };
 
